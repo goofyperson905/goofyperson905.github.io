8aabc5e6391ee3f9749ac9eb1e71a4aa1b40ac3a
goofyperson905 <129344753+goofyperson905@users.noreply.github.com>
2023-03-29 17:43:05 -0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function start() {
+    document.title = "LOL is this you?";
+    const elementMessage = document.getElementById("element_message");
+    element_message.remove();
+    const element_main = document.getElementById("main");
+    element_main.remove();
+    window.open("screen.html");
+}
